d0d5feb3a8a2254e7cf6dc4915db61478e0c2a56
Cozen31 <kompelienv@gmail.com>
2022-02-24 09:58:40 +0100

    First function done

---
+++ Javascript/prosjekt-0/first.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} x
+ * @param {number} y
+ */
+
+function sum(x, y) {
+    return x + y;
+}
+
+function sum2() {
+    console.log("Hello World");
+}
+
+console.log(sum(1,3));
+console.log(sum2(1,3));
+
